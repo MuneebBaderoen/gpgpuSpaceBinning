0c35c5a02d472635cb0a01c5954a950db9a4adb8
Muneeb Baderoen <mbaderoen@snoek.cs.uct.ac.za>
2013-05-16 17:17:40 +0200

    End of day one. file read in

--- main.cpp
+++ main.cpp
@@ -4,26 +4,56 @@
 #include <fstream>
 #include <string>
 
+struct Asteroid{
+	float x, y, value;
+};
 
-char * word;
+struct Point{
+	float x, y;
+};
 
 
-void readFile(const char* filename){
+
+
+Asteroid * asteroids;
+Point ship;
+Point baseStation;
+
+unsigned long long numAsteroids;
+
+Asteroid readFile(const char* filename){
 	using namespace std;
 
 	cout<<"in readfile"<<endl;
 
 	ifstream inFile(filename, ios::in | ios::binary);
-	char line [sizeof(float)*8];//3*sizeof(float)]; 
+	char line [sizeof(float)*12];//3*sizeof(float)]; 
 	//string s = "";
 	if(inFile.is_open()){
 		inFile.seekg (0, ios::beg);
+		//read base station position
+		float f;
+		inFile.read(reinterpret_cast<char*>(&baseStation.x), sizeof(float));
+		inFile.read(reinterpret_cast<char*>(&baseStation.y), sizeof(float));
+		inFile.read(reinterpret_cast<char*>(&f), sizeof(float));
+
+		int pos = inFile.tellg ();		
+
+		inFile.seekg (0, ios::end);
+   		numAsteroids = (unsigned long long)(((int)inFile.tellg())-pos)/12;   		
+    	inFile.seekg (pos, ios::beg);
+
+
+		asteroids = new Asteroid [numAsteroids*sizeof(Asteroid)];
+
+		int count;
+
 		while(inFile.good()){	
 
-			float f;
+			
 			inFile.read(reinterpret_cast<char*>(&f), sizeof(f));
 
-			cout<<"Float val: "<<f<<" "<<2*f<<endl;
+			//cout<<"Float val: "<<f<<" "<<2*f<<endl;
 
 
 		}
@@ -48,10 +78,10 @@ int main(int argc, char** argv){
 	printf("num %i\n", 8);
 	using namespace std;
 	string fname = "s_data_02.scan";
-	// /cout<<"Word: "<<word<<endl;
+	
 
 	readFile(fname.c_str());
-	//cout<<"Word2: "<<word<<endl;
+	
 
 
 }
